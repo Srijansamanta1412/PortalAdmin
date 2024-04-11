c4ec62ad370df8f32a3bd7798bb9d7941489f004
Srijan Samanta <srijansamanta1412@gmail.com>
2024-04-12 04:07:40 +0530

    Last Try

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,21 +4,21 @@ import './index.css';
 import AppNew from './AppNew.js';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './Navbar.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter ,Routes, Route} from 'react-router-dom';
 import User from './User.js';
 import EachUser from './EachUser.js';
 import NewUser from './NewUser.jsx';
 
 function Main() {
     return (
-        <Router>
+        <BrowserRouter >
             <Routes>
-                <Route path="/PortalAdmin" element={<AppNew />} />
+                <Route exact path="/PortalAdmin" element={<AppNew />} />
                 <Route path="/user" element={<User />} />
                 <Route path="/user/test" element={<EachUser />} />
                 <Route path="/users/new" element={<NewUser />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 export default Main;
