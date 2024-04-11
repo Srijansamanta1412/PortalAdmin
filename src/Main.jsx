722cecef1998fcd1ff8c9b8c1f570cfe82dccedf
Srijan Samanta <srijansamanta1412@gmail.com>
2024-04-12 03:13:59 +0530

    Routing

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import AppNew from './AppNew.js';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './Navbar.jsx';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import User from './User.js';
 import EachUser from './EachUser.js';
 import NewUser from './NewUser.jsx';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-function Main(){
-    return(
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppNew />}>
-        </Route>
-        <Route path="/user" element={<User/>}></Route>
-        <Route path="/user/test" element={<EachUser/>}></Route>
-        <Route path="/users/new" element={<NewUser/>}></Route>
-      </Routes>
-    </BrowserRouter>
-    </>
-)
+
+function Main() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/PortalAdmin" element={<AppNew />} />
+                <Route path="/user" element={<User />} />
+                <Route path="/user/test" element={<EachUser />} />
+                <Route path="/users/new" element={<NewUser />} />
+            </Routes>
+        </Router>
+    );
 }
 export default Main;
 
